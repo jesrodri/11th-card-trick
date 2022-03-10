b0264ae44c57c5785d24d2950f95f14464b5f40a
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-03-10 19:26:27 -0300

    extracted CardRow component

---
+++ src/components/CardRow.js
@@ -0,0 +1,15 @@
+const CardRow = ({ deck }) => {
+  return(
+    <div className="cards--container">
+      {deck.map(row => (
+      <div className="cards--row">
+        {row.map(({value, suit}) => (
+          <div className={`card card--${suit}`} key={`${value}-${suit}`}>{value}{suit}</div>
+        ))}
+      </div>
+      ))}
+    </div>
+  );
+}
+
+export default CardRow;
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,22 +1,11 @@
+import CardRow from './CardRow';
 import { trickDeck } from '../logic/deck';
 
 const Deck = () => {
-
-  function cardRow(deck) {
-    return(
-      deck.map(row => (
-      <div className="cards--row">
-        {row.map(({value, suit}) => (
-          <div className={`card card--${suit}`} key={`${value}-${suit}`}>{value}{suit}</div>
-        ))}
-      </div>
-      ))
-    );
-  };
   
   return(
     <div className="cards" data-testid="deck">
-        {cardRow( trickDeck )};
+        <CardRow deck={trickDeck} />;
     </div>
   );
 };
